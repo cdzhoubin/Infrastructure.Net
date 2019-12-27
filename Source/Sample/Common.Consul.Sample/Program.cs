8bde6db25f309a6b692bdd5e533aa33d34d16de5
cdzhoubin <cdzhoubin@163.com>
2019-12-27 11:28:19 +0800

    增加Consul主从调用样例

---
+++ Source/Sample/Common.Consul.Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Consul;
+using System;
+using System.Threading.Tasks;
+using Zhoubin.Infrastructure.Common.Consul.Leader;
+using Zhoubin.Infrastructure.Common.CoreConsole;
+
+namespace Common.Consul.Sample
+{
+    /// <summary>
+    /// 主从选举事例
+    /// 采用命令行本机启用Consul服务，命令如下：
+    /// consul.exe agent --ui --dev
+    /// </summary>
+    class Program
+    {
+        public static ConsulLeaderService consulLeaderService;
+        static void Main(string[] args)
+        {
+            using (IConsulClient client = new ConsulClient())
+            {
+                consulLeaderService = new ConsulLeaderService(client, "Sample");
+                consulLeaderService.Start();
+                HostTool.Host<SampleSingleService>(args);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Source/Sample/Common.Consul.Sample/SampleSingleService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using Zhoubin.Infrastructure.Common.CoreConsole;
+
+namespace Common.Consul.Sample
+{
+    public class SampleSingleService : HostedServiceBase<SampleSingleService>
+    {
+        public SampleSingleService(ILogger<SampleSingleService> logger) : base(logger, 1000)
+        {
+        }
+
+        protected override void DoWork()
+        {
+           if(Program.consulLeaderService.IsLeader)
+            {
+                Logger.LogInformation("Run Leader DoWork:" + DateTime.Now.ToString());
+
+            } else
+            {
+                Logger.LogInformation("DoWork:" + DateTime.Now.ToString());
+            }
+            
+        }
+    }
+}
